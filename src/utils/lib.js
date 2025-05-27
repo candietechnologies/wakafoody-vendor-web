a179b83a9d264e1c298fa2302691bcc1b630a423
candie <adekanmi.adewumi@straitpay.com>
2025-05-27 13:50:36 +0100

    upload url update

--- src/utils/lib.js
+++ src/utils/lib.js
@@ -1,2 +1,2 @@
 export const url = `${import.meta.env.VITE_API_URL}/api`;
-export const uploadUrl = "https://image-upload-production-3924.up.railway.app";
+export const uploadUrl = "https://image-upload-rfcg.onrender.com";
